4af26b106aad68ac45074af36df8881dd0fea3ec
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2023-03-02 23:00:19 +0100

    debug true

--- it_project_task_manager/settings.py
+++ it_project_task_manager/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-DEBUG = "RENDER" not in os.environ
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+# DEBUG = "RENDER" not in os.environ
 
 
 ALLOWED_HOSTS = ["127.0.0.1"]
